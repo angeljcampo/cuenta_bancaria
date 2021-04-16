1f0d7f74a4571dbdcd9212b46f860d24b985bd4f
Angel <angeljcampo@gmail.com>
2021-04-15 20:08:12 -0400

    first commit

---
+++ cuenta_bancaria.rb
@@ -0,0 +1,24 @@
+class CuentaBancaria
+  attr_accessor :nombre_de_usuario
+  attr_reader :numero_cuenta, :vip
+
+  def initialize(nombre_de_usuario,numero_cuenta, vip)
+    @nombre_de_usuario = nombre_de_usuario
+    @vip = vip
+    
+    if numero_cuenta.digits.count == 8
+      @numero_cuenta = numero_cuenta
+    else
+    raise RangeError.new('El numero de cuenta es incorrecto')
+    end
+  end
+
+  def numero_de_cuenta
+    if self.vip == 1
+      "1-#{self.numero_cuenta}"
+    else
+      "0-#{self.numero_cuenta}"
+    end
+  end
+end
+
